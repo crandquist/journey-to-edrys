3ec2cc60411a33d2b0f28ae17c6c244ba774b295
Madi <crandquist@gmail.com>
2020-06-09 14:32:38 -0700

    Moved Engine() and Map() over to scenes.py and deleted engine.py

--- scenes.py
+++ scenes.py
@@ -1,5 +1,13 @@
 from sys import exit
 
+class Engine(object):
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+    
+    def play(self):
+        pass
+
 class Scene(object):
 
     def __init__(self):
@@ -58,4 +66,31 @@ class Beach(Scene):
 class Final(Scene):
     
     def enter(self):
+        pass
+
+class Finished(Scene):
+
+    def enter(self):
+        print("FILL THIS IN WITH SOMETHING AT SOME POINT")
+
+class Map(object):
+    
+    scenes = {'garden': Garden(),
+            'forest' : Forest(),
+            'well' : Well(),
+            'cabin' : Cabin(),
+            'shed' : Shed(),
+            'bridge' : Bridge(),
+            'cave' : Cave(),
+            'beach' : Beach(),
+            'finished' : Finished()
+    }
+
+    def __init__(self, starting_scene):
+        self.starting_scene = starting_scene
+
+    def next_scene(self):
+        pass
+
+    def opening_scene(self):
         pass
