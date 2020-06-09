b9726e87544eac8eda482218d342ebf72dac9ddc
Madi <crandquist@gmail.com>
2020-06-09 12:35:53 -0700

    Finished rough sketch of all scenes in brainstorming and created Scene class and all individual scene classes in scenes.py

---
+++ scenes.py
@@ -0,0 +1,32 @@
+class Scene(object):
+    pass
+
+class Kitchen(Scene):
+    pass
+
+class Garden(Scene):
+    pass
+
+class Forest(Scene):
+    pass
+
+class Well(Scene):
+    pass
+
+class Cabin(Scene):
+    pass
+
+class Shed(Scene):
+    pass
+
+class Bridge(Scene):
+    pass
+
+class Cave(Scene):
+    pass
+
+class Beach(Scene):
+    pass
+
+class Final(Scene):
+    pass
