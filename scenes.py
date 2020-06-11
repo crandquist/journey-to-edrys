d8ed8d80857163758544d18f3ca58f640fcdce5d
Madi <crandquist@gmail.com>
2020-06-11 10:26:40 -0700

    Started writing Cabin().

--- scenes.py
+++ scenes.py
@@ -268,12 +268,42 @@ class Cabin(Scene):
 
     def enter(self):
         print("""
-            \nScene context will go here.
+            \nYou pick up the key and walk back over to the cabin, the wolf following behind you. You unlock the door and push it open, looking around before you step inside. It's a small cabin with only one room. Along one wall is a bed barely big enough for a single adult. The far wall has a fireplace with a cooking pot hanging over what you suspect are warm coals. There is a round table with two chairs that is cluttered with dishes, parchment, and other items. Some of the items on the table are recognizable, but others are strange. There is a jar filled with a clear liquid and what looks to be organs from several small animals. There are herbs and twigs scattered across the table as well.
+            \nThe wolf seems uneasy at your side. You look to him as you step into the cabin, but he makes no move to follow you. 
+            \nYou shrug and turn to look closer around the room. The Mage had said that there would be new information about Edrys here, but nothing seems to stand out as something you are supposed to be looking at.
+            \nThe parchment scattered across the table is blank. There is a quill and ink pot there as well. Likely used to write the notes you have found already. There is a small side table next to the bed with a leatherbound book on it. You pick up the book and open it to the first page. It appears to be a journal, written in The Mage's handwriting.
+            \nDo you read the journal?
             """)
-        choice = input("> ")
+        answer = input("> ")
+        choice = answer.lower()
 
-        if "choice option" in choice:
-            print("\nConsequence goes here.")
+        if choice == "yes":
+            print("""
+                \nHarvest 30, 325
+                \nI met the most delightful creature today, followed me out of the forest. I didn't even realize anyone was behind me until I hit the edge of the wards around the cabin. She is graceful and fierce. She thought me an intruder in the forest and followed me to ensure that I stayed away in the future.
+                \nIt was a minor misunderstanding and I was able to explain to her that I am here to protect the forest and all of its inhabitants. She was willing to listen and allowed me close enough to her to see that she had an injury on her right leg. 
+                \nI healed the injury for her as a show of good faith and she stayed in the cabin with me while I prepared dinner. She left not too long after.
+                \nHopefully this will not be the last I see of her. I only now realized that I didn't get her name.
+                """)
+            input("[enter]")
+            print("""
+                \nHarvest 39, 325
+                \nMy friend from the forest followed me home again today. She found me as I was finishing up the process of getting rid of a group of poachers who had been hunting the cestrals on the East side.
+                \nI appologized to her for my bad manners the other day and asked for her name. She let me know that I could call her Edrys. What a lovely name.
+                """)
+            input("[enter]")
+            print("""
+                \nThere are many entries along these same lines mentioning Edrys through the first two thirds of the journal. You flip through them but do not spend the time to read them in their entirety. You stop toward the end of the journal on a longer entry.
+                \n
+                \nGrowth 3, 326
+                \nEdrys has been living in the cabin with me for a few weeks now. We have settled into a routine. She helps me gather food and herbs and other potions ingredients in the morning and accompanies me on my patrols of the forest.
+                \nI had been worried that she would not be able to cope with the destruction of her home in the forest. I was even more worried that she would resent me for not being quick enough to save it. I admit, I had been too focused on worrying about her unnecessarily instead of working with her as a team like we do now. She has certainly scolded me in the past weeks for not seeing that she is more than capable of protecting herself, maybe even moreso than anything I could do to protect her.
+                \nI have told her that she can have the shed in the back to herself, but she refuses to use it. She let me know that she prefers to be in the cabin with me. She likes the smells and the warmth and the company.
+                \nShe likes to wander in the evenings. While I do my reading and correspondence she has been exploring parts of the valley that she had never known existed while she was living in the forest. She's never gone long. I think she is growing as fond of me as I am of her.
+                \nI am shocked to find how much brighter my life is with Edrys. I have never begrudged the solitary job of watching over the forest. I love the creatures under my protection and find pride in the work I do.
+                \nStill.
+                \nIt is so nice not to be alone.
+                """)
             #return statement moving game forward
 
         elif "losing choice option" in choice:
